8612885df4d478880b806425e8eccd69ecafc1c6
Unknown <eunsikkim83@gmail.com>
2017-02-25 01:19:52 -0500

    UI add and input/display name

--- 072225/app/src/main/java/com/example/ergokim/a072225/MainActivity.java
+++ 072225/app/src/main/java/com/example/ergokim/a072225/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.ergokim.a072225;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonOk = (Button) findViewById(R.id.button_ok);
+        final EditText editName = (EditText) findViewById(R.id.edit_name);
+        final TextView textNames = (TextView) findViewById(R.id.text_names);
+
+        /**
+         * 할일
+         * 1. 버튼 클릭하면  입력된 이름을 텍스트뷰포 보내기
+         * 2. 입력필드 비우기
+         *
+         */
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editName.getText().toString();
+                Log.i("###", "onClick: name = " + name);
+                textNames.setText(name);
+                editName.setText(null);
+            }
+        });
     }
 }
