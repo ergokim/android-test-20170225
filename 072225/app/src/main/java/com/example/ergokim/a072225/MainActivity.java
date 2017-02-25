caf445223ae7152da294625be981aa2a1918ca12
Unknown <eunsikkim83@gmail.com>
2017-02-25 01:41:04 -0500

    add inputName Method

--- 072225/app/src/main/java/com/example/ergokim/a072225/MainActivity.java
+++ 072225/app/src/main/java/com/example/ergokim/a072225/MainActivity.java
@@ -10,7 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText editName;
+    TextView textNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,36 +23,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button buttonOk = (Button) findViewById(R.id.button_ok);
-        final EditText editName = (EditText) findViewById(R.id.edit_name);
-        final TextView textNames = (TextView) findViewById(R.id.text_names);
+        editName = (EditText) findViewById(R.id.edit_name);
+        textNames = (TextView) findViewById(R.id.text_names);
 
-        /**
-         * 할일
-         * 1. 버튼 클릭하면  입력된 이름을 텍스트뷰포 보내기
-         * 2. 입력필드 비우기
-         *
-         */
+
         buttonOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name = editName.getText().toString();
-                Log.i("###", "onClick: name = " + name);
-                textNames.append("\n" + name);
-                editName.setText(null);
+                inputName();
             }
         });
 
         editName.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if(EditorInfo.IME_ACTION_DONE == actionId){
-                    String name = editName.getText().toString();
-                    Log.i("###", "onClick: name = " + name);
-                    textNames.append("\n" + name);
-                    editName.setText(null);
+                if (EditorInfo.IME_ACTION_DONE == actionId) {
+                    inputName();
                 }
                 return false;
             }
         });
+    }
+
+    /**
+     * 버튼 클릭하면  입력된 이름을 텍스트뷰로 보내기<br>
+     * 입력필드 비우기
+     */
+    private void inputName() {
+        String name = editName.getText().toString();
+        Log.i("###", "inputName: name = " + name);
+        textNames.append("\n" + name);
+        editName.setText(null);
     }
 }
